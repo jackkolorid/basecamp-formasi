18bed4fb3e66cbaa24069ec3b7db548b09fb0f7c
alhifnywahid <hifnycarlytosss@gmail.com>
2024-09-06 18:06:23 +0700

    d

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,9 +1,20 @@
-// config/db.js
 import mongoose from "mongoose";
 
+let isConnected = false; // Track the connection status
+
 const connectDB = async () => {
+	if (isConnected) {
+		// Jika sudah terhubung, gunakan koneksi yang ada
+		console.log("Menggunakan koneksi database yang sudah ada.");
+		return;
+	}
+
 	try {
-		await mongoose.connect(process.env.MONGO_URI);
+		await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		isConnected = true; // Set flag koneksi ke true
 		console.log("MongoDB Connected...");
 	} catch (err) {
 		console.error("ERROR : ", err.message);
